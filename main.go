83cfd59f50e88e6e8aeb213c2fe459b013dac60e
Danny Krämer <d.kraemer@dotsource.de>
2024-03-24 19:49:18 +0100

    read file

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.ReadFile("test.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	scanner := bufio.NewScanner(file)
-
 	a := app.New()
 	w := a.NewWindow("gim")
 
-	w.SetContent(widget.NewLabel(scanner.Text()))
+	w.SetContent(widget.NewLabel(string(file)))
 	w.ShowAndRun()
 }
 
